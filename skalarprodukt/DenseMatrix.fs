3637a5fb7c496df43e4a683070c4b50eb206d936
Evgeniy Andreev (gsomix) <gsomix@gmail.com>
2016-10-06 14:12:27 +0400

    Add initial implementation of dense matrix

---
+++ skalarprodukt/DenseMatrix.fs
@@ -0,0 +1,40 @@
+﻿namespace skalarprodukt
+
+module DenseMatrix =
+    type DenseMatrix<'t> = 
+        {
+            NumRows : int
+            NumCols : int
+            Data : 't array
+        }
+
+        static member inline map2 f x y =
+            if x.NumCols <> y.NumCols || x.NumRows <> y.NumRows
+            then raise(System.ArithmeticException())
+            {
+                NumRows = x.NumRows
+                NumCols = x.NumCols
+                Data = Array.map2 f x.Data y.Data
+            }
+
+        static member inline (+) (x, y) = 
+            DenseMatrix<_>.map2 (+) x y
+
+        static member inline (-) (x, y) =
+            DenseMatrix<_>.map2 (-) x y
+
+        static member inline (.*) (x, y) =
+            DenseMatrix<_>.map2 (*) x y
+
+        static member inline (./) (x, y) =
+            DenseMatrix<_>.map2 (/) x y
+
+    let ofArray nrows ncols data =
+        {
+            NumRows = nrows
+            NumCols = ncols
+            Data = data
+        }
+
+    let inline (.*) (x, y) = x .* y
+    let inline (./) (x, y) = x ./ y
---
+++ skalarprodukt/Script.fsx
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "DenseMatrix.fs"
+open skalarprodukt
+
+let m1 = DenseMatrix.ofArray 2 2 [|1; 0; 0; 1|]
+let m2 = DenseMatrix.ofArray 2 2 [|0; -1; -1; 0|]
+
+let m3 = m1 + m2
+let m4 = m1 .* m2
